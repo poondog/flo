0303b43722257e507c28f73e2e5b1df863632e31
poondog <markj338@gmail.com>
2013-12-28 20:47:30 +1100

    Initial commit
    Signed-off-by: poondog <markj338@gmail.com>

---
+++ arch/arm/kernel/mako_hotplug.c
@@ -0,0 +1,290 @@
+/*
+ * Copyright (c) 2013, Francisco Franco <franciscofranco.1990@gmail.com>. 
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Simple no bullshit hot[un]plug driver for SMP
+ */
+
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/cpu.h>
+#include <linux/workqueue.h>
+#include <linux/sched.h>
+#include <linux/timer.h>
+#include <linux/earlysuspend.h>
+#include <linux/cpufreq.h>
+#include <linux/delay.h>
+#include <linux/hotplug.h>
+#include <linux/input/lge_touch_core.h>
+#include <linux/input.h>
+#include <linux/jiffies.h>
+#include <mach/cpufreq.h>
+
+#define DEFAULT_FIRST_LEVEL 60
+#define DEFAULT_SUSPEND_FREQ 702000
+#define DEFAULT_CORES_ON_TOUCH 2
+#define HIGH_LOAD_COUNTER 20
+#define TIMER HZ
+
+#define MIN_TIME_CPU_ONLINE HZ
+
+static struct cpu_stats
+{
+    unsigned int default_first_level;
+    unsigned int suspend_frequency;
+    unsigned int cores_on_touch;
+    unsigned int counter[2];
+	unsigned long timestamp[2];
+} stats = {
+	.default_first_level = DEFAULT_FIRST_LEVEL,
+    .suspend_frequency = DEFAULT_SUSPEND_FREQ,
+    .cores_on_touch = DEFAULT_CORES_ON_TOUCH,
+    .counter = {0},
+	.timestamp = {0},
+};
+
+struct cpu_load_data {
+	u64 prev_cpu_idle;
+	u64 prev_cpu_wall;
+};
+
+static DEFINE_PER_CPU(struct cpu_load_data, cpuload);
+
+static struct workqueue_struct *wq;
+static struct workqueue_struct *pm_wq;
+static struct delayed_work decide_hotplug;
+static struct work_struct resume;
+static struct work_struct suspend;
+
+static inline int get_cpu_load(unsigned int cpu)
+{
+	struct cpu_load_data *pcpu = &per_cpu(cpuload, cpu);
+	struct cpufreq_policy policy;
+	u64 cur_wall_time, cur_idle_time;
+	unsigned int idle_time, wall_time;
+	unsigned int cur_load;
+
+	cpufreq_get_policy(&policy, cpu);
+
+	cur_idle_time = get_cpu_idle_time(cpu, &cur_wall_time, true);
+
+	wall_time = (unsigned int) (cur_wall_time - pcpu->prev_cpu_wall);
+	pcpu->prev_cpu_wall = cur_wall_time;
+
+	idle_time = (unsigned int) (cur_idle_time - pcpu->prev_cpu_idle);
+	pcpu->prev_cpu_idle = cur_idle_time;
+
+	if (unlikely(!wall_time || wall_time < idle_time))
+		return 0;
+
+	cur_load = 100 * (wall_time - idle_time) / wall_time;
+
+	return (cur_load * policy.cur) / policy.max;
+}
+
+static void cpu_revive(unsigned int cpu)
+{
+	cpu_up(cpu);
+	stats.timestamp[cpu - 2] = jiffies;
+}
+
+static void cpu_smash(unsigned int cpu)
+{
+	/*
+	 * Let's not unplug this cpu unless its been online for longer than
+	 * 1sec to avoid consecutive ups and downs if the load is varying
+	 * closer to the threshold point.
+	 */
+	if (time_is_after_jiffies(stats.timestamp[cpu - 2] + MIN_TIME_CPU_ONLINE))
+		return;
+
+	cpu_down(cpu);
+	stats.counter[cpu - 2] = 0;
+}
+
+static void __ref decide_hotplug_func(struct work_struct *work)
+{
+    int cpu;
+	//int i;
+	int cpu_nr = 2;
+	unsigned int cur_load;
+	
+	/*if (_ts->ts_data.curr_data[0].state == ABS_PRESS)
+	{
+		for (i = num_online_cpus(); i < stats.cores_on_touch; i++)
+		{
+			if (cpu_is_offline(i))
+			{
+				cpu_up(i);
+				stats.timestamp[i-2] = ktime_to_ms(ktime_get());
+			}
+		}
+		goto re_queue;
+	}*/
+
+    for_each_online_cpu(cpu) 
+    {
+		cur_load = get_cpu_load(cpu);
+
+		if (cur_load >= stats.default_first_level)
+		{
+			if (likely(stats.counter[cpu] < HIGH_LOAD_COUNTER))    
+				stats.counter[cpu] += 2;
+
+			if (cpu_is_offline(cpu_nr) && stats.counter[cpu] >= 10)
+				cpu_revive(cpu_nr);
+		}
+
+		else
+		{
+			if (stats.counter[cpu])
+				--stats.counter[cpu];
+
+			if (cpu_online(cpu_nr) && stats.counter[cpu] < 10)
+				cpu_smash(cpu_nr);
+		}
+
+		cpu_nr++;
+
+		if (cpu)
+			break;
+	}
+
+//re_queue:	
+    queue_delayed_work(wq, &decide_hotplug, msecs_to_jiffies(TIMER));
+}
+
+static void suspend_func(struct work_struct *work)
+{
+	int cpu;
+
+    /* cancel the hotplug work when the screen is off and flush the WQ */
+	flush_workqueue(wq);
+    cancel_delayed_work_sync(&decide_hotplug);
+	cancel_work_sync(&resume);
+
+    pr_info("Early Suspend stopping Hotplug work...\n");
+    
+    for_each_online_cpu(cpu) 
+    {
+        if (cpu) 
+            cpu_down(cpu);
+    }
+
+	/* reset the counters so that we start clean next time the display is on */
+    stats.counter[0] = 0;
+    stats.counter[1] = 0;
+
+    /* cap max frequency to 702MHz by default */
+    msm_cpufreq_set_freq_limits(0, MSM_CPUFREQ_NO_LIMIT, 
+            stats.suspend_frequency);
+    pr_info("Cpulimit: Early suspend - limit cpu%d max frequency to: %dMHz\n",
+            0, stats.suspend_frequency/1000);	
+}
+
+static void __ref resume_func(struct work_struct *work)
+{
+	int cpu;
+
+	cancel_work_sync(&suspend);
+
+	/* restore max frequency */
+    msm_cpufreq_set_freq_limits(0, MSM_CPUFREQ_NO_LIMIT, MSM_CPUFREQ_NO_LIMIT);
+    pr_info("Cpulimit: Late resume - restore cpu%d max frequency.\n", 0);
+
+    /* online all cores when the screen goes online */
+    for_each_possible_cpu(cpu) 
+    {
+        if (cpu) 
+            cpu_up(cpu);
+    }
+    
+    pr_info("Late Resume starting Hotplug work...\n");
+    queue_delayed_work(wq, &decide_hotplug, HZ * 2);	
+}
+
+static void mako_hotplug_early_suspend(struct early_suspend *handler)
+{	 
+    queue_work(pm_wq, &suspend);
+}
+
+static void mako_hotplug_late_resume(struct early_suspend *handler)
+{  
+	queue_work(pm_wq, &resume);
+}
+
+static struct early_suspend mako_hotplug_suspend =
+{
+    .level = EARLY_SUSPEND_LEVEL_BLANK_SCREEN + 1,
+	.suspend = mako_hotplug_early_suspend,
+	.resume = mako_hotplug_late_resume,
+};
+
+/* sysfs functions for external driver */
+void update_first_level(unsigned int level)
+{
+    stats.default_first_level = level;
+}
+
+void update_suspend_frequency(unsigned int freq)
+{
+    stats.suspend_frequency = freq;
+}
+
+void update_cores_on_touch(unsigned int num)
+{
+    stats.cores_on_touch = num;
+}
+
+unsigned int get_first_level()
+{
+    return stats.default_first_level;
+}
+
+unsigned int get_suspend_frequency()
+{
+    return stats.suspend_frequency;
+}
+
+unsigned int get_cores_on_touch()
+{
+    return stats.cores_on_touch;
+}
+/* end sysfs functions from external driver */
+
+int __init mako_hotplug_init(void)
+{
+	pr_info("Mako Hotplug driver started.\n");
+
+    wq = alloc_ordered_workqueue("mako_hotplug_workqueue", 0);
+    
+    if (!wq)
+        return -ENOMEM;
+
+	pm_wq = alloc_workqueue("pm_workqueue", 0, 1);
+    
+    if (!pm_wq)
+        return -ENOMEM;
+
+	stats.timestamp[0] = jiffies;
+	stats.timestamp[1] = jiffies;    
+
+    INIT_DELAYED_WORK(&decide_hotplug, decide_hotplug_func);
+	INIT_WORK(&resume, resume_func);
+	INIT_WORK(&suspend, suspend_func);
+    queue_delayed_work(wq, &decide_hotplug, HZ*25);
+    
+    register_early_suspend(&mako_hotplug_suspend);
+    
+    return 0;
+}
+late_initcall(mako_hotplug_init);
+
---
+++ drivers/thermal/msm_thermal.c
@@ -0,0 +1,207 @@
+/* Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/mutex.h>
+#include <linux/msm_tsens.h>
+#include <linux/workqueue.h>
+#include <linux/cpu.h>
+#include <linux/cpufreq.h>
+#include <linux/msm_tsens.h>
+#include <linux/msm_thermal.h>
+#include <linux/platform_device.h>
+#include <linux/of.h>
+#include <mach/cpufreq.h>
+
+static struct cpus {
+	bool throttling;
+	int thermal_steps[5];
+	uint32_t limited_max_freq;
+	unsigned int max_freq;
+	struct cpufreq_policy policy;
+} cpu_stats = {
+	.throttling = false,
+	.thermal_steps = {702000, 1134000, 1242000, 1350000},
+	.limited_max_freq = UINT_MAX,
+};
+
+unsigned int temp_threshold = 70;
+module_param(temp_threshold, int, 0755);
+
+static struct msm_thermal_data msm_thermal_info;
+
+static struct workqueue_struct *wq;
+static struct delayed_work check_temp_work;
+
+unsigned short get_threshold(void)
+{
+	return temp_threshold;
+}
+
+static int  msm_thermal_cpufreq_callback(struct notifier_block *nfb,
+		unsigned long event, void *data)
+{
+	struct cpufreq_policy *policy = data;
+
+	switch (event) {
+	case CPUFREQ_INCOMPATIBLE:
+		cpufreq_verify_within_limits(policy, policy->min,
+			cpu_stats.limited_max_freq);
+		break;
+	}
+	return NOTIFY_OK;
+}
+
+static struct notifier_block msm_thermal_cpufreq_notifier = {
+	.notifier_call = msm_thermal_cpufreq_callback,
+};
+
+static void limit_cpu_freqs(uint32_t max_freq)
+{
+    int cpu;
+
+	if (cpu_stats.limited_max_freq == max_freq)
+		return;
+
+	cpu_stats.limited_max_freq = max_freq;
+    
+	/* Update new limits */
+	get_online_cpus();
+	for_each_online_cpu(cpu)
+	{
+		cpufreq_update_policy(cpu);
+		pr_info("%s: Setting cpu%d max frequency to %d\n",
+				KBUILD_MODNAME, cpu, cpu_stats.limited_max_freq);
+	}
+	put_online_cpus();
+}
+
+void update_max_freq_buffer(void)
+{
+	if (!cpu_stats.throttling)
+	{
+		cpufreq_get_policy(&cpu_stats.policy, 0);
+		cpu_stats.max_freq = cpu_stats.policy.max;
+	}
+}
+
+static void check_temp(struct work_struct *work)
+{
+	struct tsens_device tsens_dev;
+	long temp = 0;
+    
+	tsens_dev.sensor_num = msm_thermal_info.sensor_id;
+	tsens_get_temp(&tsens_dev, &temp);
+
+	/* most of the time the device is not hot so reschedule early */
+	if (likely(temp < temp_threshold))
+	{
+		if (unlikely(cpu_stats.throttling))
+		{
+			limit_cpu_freqs(cpu_stats.max_freq);
+			cpu_stats.throttling = false;
+		}
+
+		goto reschedule;
+	}
+
+	/* 
+	 * Get the max freq before we throttle so that we can re-apply it after
+	 * the device cools down
+	 */
+	update_max_freq_buffer();
+
+	if (temp >= (temp_threshold + 20))
+		limit_cpu_freqs(cpu_stats.thermal_steps[0]);
+
+	else if (temp >= (temp_threshold + 15))
+		limit_cpu_freqs(cpu_stats.thermal_steps[1]);
+    
+	else if (temp >= (temp_threshold + 10))
+		limit_cpu_freqs(cpu_stats.thermal_steps[2]);
+
+	else if (temp >= temp_threshold)
+		limit_cpu_freqs(cpu_stats.thermal_steps[3]);
+
+	cpu_stats.throttling = true;
+
+reschedule:
+	queue_delayed_work(wq, &check_temp_work, HZ);
+}
+
+int __devinit msm_thermal_init(struct msm_thermal_data *pdata)
+{
+	int ret = 0;
+    
+	BUG_ON(!pdata);
+	BUG_ON(pdata->sensor_id >= TSENS_MAX_SENSORS);
+	memcpy(&msm_thermal_info, pdata, sizeof(struct msm_thermal_data));
+    
+	wq = alloc_workqueue("msm_thermal_workqueue", WQ_FREEZABLE | WQ_UNBOUND, 0);
+    
+    if (!wq)
+        return -ENOMEM;
+
+	cpufreq_register_notifier(&msm_thermal_cpufreq_notifier,
+			CPUFREQ_POLICY_NOTIFIER);
+    
+	INIT_DELAYED_WORK(&check_temp_work, check_temp);
+	queue_delayed_work(wq, &check_temp_work, HZ*30);
+    
+	return ret;
+}
+
+static int __devinit msm_thermal_dev_probe(struct platform_device *pdev)
+{
+	int ret = 0;
+	char *key = NULL;
+	struct device_node *node = pdev->dev.of_node;
+	struct msm_thermal_data data;
+    
+	memset(&data, 0, sizeof(struct msm_thermal_data));
+	key = "qcom,sensor-id";
+	ret = of_property_read_u32(node, key, &data.sensor_id);
+	if (ret)
+		goto fail;
+	WARN_ON(data.sensor_id >= TSENS_MAX_SENSORS);
+    
+fail:
+	if (ret)
+		pr_err("%s: Failed reading node=%s, key=%s\n",
+		       __func__, node->full_name, key);
+	else
+		ret = msm_thermal_init(&data);
+    
+	return ret;
+}
+
+static struct of_device_id msm_thermal_match_table[] = {
+	{.compatible = "qcom,msm-thermal"},
+	{},
+};
+
+static struct platform_driver msm_thermal_device_driver = {
+	.probe = msm_thermal_dev_probe,
+	.driver = {
+		.name = "msm-thermal",
+		.owner = THIS_MODULE,
+		.of_match_table = msm_thermal_match_table,
+	},
+};
+
+int __init msm_thermal_device_init(void)
+{
+	return platform_driver_register(&msm_thermal_device_driver);
+}
