cac49bda1abb7a22f5dd577a469ade6845eb660d
poondog <markj338@gmail.com>
2014-01-03 12:33:15 +1100

    Revert "staging: android: lowmemorykiller: Don't count reserved free memory"
    This reverts commit 3ade0309f6b460a7831a2ac50bba15669f79b3f0.

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -35,7 +35,6 @@
 #include <linux/mm.h>
 #include <linux/oom.h>
 #include <linux/sched.h>
-#include <linux/swap.h>
 #include <linux/rcupdate.h>
 #include <linux/notifier.h>
 #include <linux/mutex.h>
@@ -246,7 +245,7 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 			return 0;
 	}
 
-	other_free = global_page_state(NR_FREE_PAGES) - totalreserve_pages;
+	other_free = global_page_state(NR_FREE_PAGES);
 	other_file = global_page_state(NR_FILE_PAGES) -
 						global_page_state(NR_SHMEM);
 
